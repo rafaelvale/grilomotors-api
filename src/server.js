cb707575714eb09962fae0048d2fc2ae57658d73
Rafael Marques Vale <rmarquev@everis.com>
2019-08-15 21:00:00 -0300

    connection string azure

--- src/server.js
+++ src/server.js
@@ -8,8 +8,11 @@ const app = express();
 const server = require('http').Server(app);
 
 mongoose.connect(
-    'mongodb+srv://rmvsolutions:lorenna123@cluster0-6fif3.azure.mongodb.net/grilomotors?retryWrites=true&w=majority',{
-         useNewUrlParser: true 
+   //'mongodb+srv://rmvsolutions:lorenna123@cluster0-6fif3.azure.mongodb.net/grilomotors?retryWrites=true&w=majority'
+   'mongodb://apimongo:QyUwj3Ayrrc0PUrFdePHBCdLppY0JTLA5lxOs8x81C3u0aQSt1ezeUVUG8wj4RoGhB0WCuktxIH6zhS9aEKEFA==@apimongo.documents.azure.com:10255/?ssl=true'
+   ,{
+     
+   useNewUrlParser: true 
         });
 
 
