c7d7eda633fd8fd712e40ec0dacf288b4857a07e
Rafael Marques do Vale <fael_vale@hotmail.com>
2019-09-25 18:05:58 -0300

    routes.js atualizado novamente

--- src/routes.js
+++ src/routes.js
@@ -17,8 +17,8 @@ const upload = multer(uploadConfig);
 
 // routes.get('/login', UsuariosController.index);
 // routes.post('/login', UsuariosController.store);
-routes.get('/carros', CarrosController.index);
-routes.post('/fotosCarros',upload.single('fotos'), CarrosController.store);
+routes.get('/FotosCarros', CarrosController.index);
+routes.post('/Carros',upload.single('fotos'), CarrosController.store);
 routes.post('/fotos/:carroID',upload.single('fotos'),FotosController.store);
 routes.get('/fotos/:carroID',FotosController.index);
 routes.post('/user/:userID/likes', LikeController.store);
