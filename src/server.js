3cc7a1854a23a07efe4a5d61b4792dffb8831f79
Rafael Marques do Vale <fael_vale@hotmail.com>
2019-09-25 19:28:29 -0300

    server.js funcionalidades

--- src/server.js
+++ src/server.js
@@ -21,8 +21,7 @@ mongoose.connect(
 app.use(cors());
 app.use(express.json());
 app.use(routes);
-server.listen(process.env.PORT || 3000, function(){
-        console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-      });
+
+server.listen(process.env.PORT || 3333);
 
 
